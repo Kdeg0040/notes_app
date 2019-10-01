b5800d70d5986a86172eb6c92fec4968b39d8d70
karlo <deguzmankarlo@gmail.com>
2019-10-01 15:38:00 +0100

    Note list view returns html string with note list items in unordered list

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -3,7 +3,7 @@
     var notelist = new NoteList()
     notelist.add("Karlo")
     notelist.add("Tom")
-    assert.isTrue(noteListView(notelist) ===
+    assert.isTrue(NoteListView(notelist) ===
     "<ul><li><div>Karlo</div></li><li><div>Tom</div></li></ul>")
   }
 
---
+++ src/note-list-view.js
@@ -0,0 +1,13 @@
+(function(exports) {
+  function NoteListView(noteList) {
+    var listArray = noteList.all;
+    var htmlArray = ["<ul>"]
+    for (let i = 0; i < listArray.length; i++) {
+      htmlArray.push("<li><div>" + listArray[i].text + "</div></li>")
+    }
+    htmlArray.push("</ul>")
+    return htmlArray.join("")
+  }
+
+  exports.NoteListView = NoteListView;
+})(this);
