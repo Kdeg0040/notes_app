823fe55ab83d3fd5de9450d5557e416ef37e4749
karlo <deguzmankarlo@gmail.com>
2019-10-01 12:07:37 +0100

    Refactor note-tests

--- note-tests.js
+++ note-tests.js
@@ -1,10 +1,16 @@
 (function(exports) {
+
+  var assert = {
+    isTrue: function(assertionToCheck) {
+      if (!assertionToCheck) {
+        throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+      }
+    }
+  };
+
   function testNoteObjectReturnsText() {
     var note = new Note("My favourite language is Javascript");
-
-    if (note.text !== "My favourite language is Javascript") {
-      throw new Error("No text found")
-    }
+    assert.isTrue(note.text === "My favourite language is Javascript");
   }
 
   testNoteObjectReturnsText();
