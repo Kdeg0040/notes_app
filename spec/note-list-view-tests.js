42324e2a2f6a3bc412d3894e0527944a9936c3bf
Student <student@Makerss-MacBook-Pro-8.local>
2019-10-03 14:53:46 +0100

    the substring is the powerhouse of the cell

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -8,5 +8,12 @@
     "<ul><li><div>NoteOne</div></li><li><div>NoteTwo</div></li></ul>")
   }
 
+  function listViewOnlyShowsTheFirst20Characters() {
+    var notelist = new NoteList()
+    notelist.add("The substring() method returns the part of the string between the start and end indexes, or to the end of the string.")
+    assert.isTrue(notelistview.html(notelist) ===
+    "<ul><li><div>The substring() meth</div></li></ul>")
+  }
   correctHTMLOutputtedFromNotelist()
+  listViewOnlyShowsTheFirst20Characters()
 })(this);
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -5,7 +5,7 @@
     var listArray = noteList.all;
     var htmlArray = ["<ul>"]
     for (let i = 0; i < listArray.length; i++) {
-      htmlArray.push("<li><div>" + listArray[i].text + "</div></li>")
+      htmlArray.push("<li><div>" + listArray[i].text.substring(0, 20) + "</div></li>")
     }
     htmlArray.push("</ul>")
     return htmlArray.join("")
